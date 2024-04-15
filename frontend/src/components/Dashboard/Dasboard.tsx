ad2892b3c131df8d7b840ed89d94847c04eaa602
Cleopha <62258879+Cleopha@users.noreply.github.com>
2024-04-15 21:12:30 +0200

    feat: add AA (#1)
    - With web3Auth and metamask adapter
    - With on WalletButton
    - With SafeAuth component only test
    
    Signed-off-by: cleopha <coline@seguret.net>

--- frontend/src/components/Dashboard/Dasboard.tsx
+++ frontend/src/components/Dashboard/Dasboard.tsx
@@ -2,16 +2,15 @@ import React from 'react';
 
 function Dashboard() {
 	return (
-		// TODO: Just an example
 		<div className="mx-2 flex size-full bg-red-500">
+			<div className="flex-1 border-l border-grey bg-yellow-500">{/* List Person */}</div>
+
 			<div className="flex flex-[3_2_0%] flex-col bg-blue-500">
 				<div className="flex-1 bg-green-500">{/* Information Vault */}</div>
 				<div className="flex flex-[5_2_0%] bg-pink-500">
-					<div className="flex-[3_2_0%] bg-purple-500">{/* Pie Chart */}</div>
-					<div className="flex-1 bg-orange-500">{/* List deposit addr */}</div>
+					<div className="flex-1 bg-orange-500">{/* Monaco view */}</div>
 				</div>
 			</div>
-			<div className="flex-1 border-l border-grey bg-yellow-500">{/* List Person */}</div>
 		</div>
 		// <div className="flex size-full flex-col bg-Black">
 		// 	<div className="p-20">
--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 
 import WalletConnectSignal from '@/assets/WalletConnectSignal.svg';
 
+import WalletButton from './WalletButton';
+
 function Header() {
 	return (
 		<div className="mx-2 flex flex-1 border-b border-grey">
-			<div className=" flex flex-[3_2_0%] flex-col justify-center">
+			<div className="mr-2 flex items-center border border-grey p-10 text-xl font-bold">Logo</div>
+
+			<div className=" flex flex-[3_2_0%] flex-col justify-center ">
 				<div className="flex  ">
 					<p className="text-xs text-secondGrey">Hi, Jessica</p>
 				</div>
@@ -13,16 +17,8 @@ function Header() {
 					<h2 className="text-2xl font-bold text-White">Welcome Back</h2>
 				</div>
 			</div>
-			<div className=" flex flex-1 items-center justify-end border-l border-grey">
-				<button
-					type="button"
-					className="flex items-center justify-center rounded-full bg-gradient-to-r from-orange-500 to-purple-500 p-px"
-				>
-					<div className="m-px flex size-full items-center justify-center rounded-full bg-black p-2 text-lg  text-white hover:bg-grey">
-						<WalletConnectSignal className="mr-1 size-7 fill-White" aria-hidden="true" />
-						Wallet
-					</div>
-				</button>
+			<div className=" flex flex-1 items-center justify-end ">
+				<WalletButton />
 			</div>
 		</div>
 	);
